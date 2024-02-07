b4ae92f0713b762fd8155c942d6c7a262924ae3b
Administrator <Administrator@DESKTOP-4J5DF41>
2024-02-07 19:21:23 +0530

    updatecode 2. 10

--- sdac/src/sdac/Main.java
+++ sdac/src/sdac/Main.java
@@ -3,5 +3,8 @@ package sdac;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("hello world");
+		System.out.println("hello world1");
+		System.out.println("hello world2");
+		System.out.println("hello world3");
 	}
 }
